aad115ae02bfc3c668d4bd71d90c181793a6ccbd
Hyunkuk <boris0815@naver.com>
2022-09-07 15:17:09 +0900

    [feature] bottom fragment, baseFragment 생성 & viewBinding 로직 추가

---
+++ app/src/main/java/com/example/wantedpreonboarding/presentation/categories/CategoriesFragment.kt
@@ -0,0 +1,20 @@
+package com.example.wantedpreonboarding.presentation.categories
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.wantedpreonboarding.R
+
+class CategoriesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_categories, container, false)
+    }
+}
