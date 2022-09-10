c473c90198d079cca09a8b08ff913295f94b5a73
Hyunkuk <boris0815@naver.com>
2022-09-10 17:53:43 +0900

    [refactor] adapter & viewModel directory 생성 및 파일이동

---
+++ app/src/main/java/com/example/wantedpreonboarding/domain/repository/SavedNewsRepository.kt
@@ -0,0 +1,11 @@
+package com.example.wantedpreonboarding.domain.repository
+
+import com.example.wantedpreonboarding.domain.model.TopNews
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * @Created by 김현국 2022/09/10
+ */
+interface SavedNewsRepository {
+    fun getSavedNewsList(): Flow<List<TopNews>>
+}
