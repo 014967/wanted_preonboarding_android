e07dce532658e233c715a9770776261a1a092ce8
Hyunkuk <boris0815@naver.com>
2022-09-10 18:00:13 +0900

    [feature] savedViewModel 생성

---
+++ app/src/main/java/com/example/wantedpreonboarding/presentation/viewmodel/SavedViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.wantedpreonboarding.presentation.viewmodel
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+/**
+ * @Created by 김현국 2022/09/10
+ */
+@HiltViewModel
+class SavedViewModel
