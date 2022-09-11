f35616a6fdc643ebc1585ecf064ab416de505dec
Hyunkuk <boris0815@naver.com>
2022-09-11 17:50:50 +0900

    [refactor] Change ArticleDao to nullable

--- app/src/main/java/com/example/wantedpreonboarding/data/source/local/dao/article/ArticleDao.kt
+++ app/src/main/java/com/example/wantedpreonboarding/data/source/local/dao/article/ArticleDao.kt
@@ -13,7 +13,7 @@ interface ArticleDao {
     suspend fun insert(article: ArticleEntity): Long
 
     @Query("SELECT * FROM articles JOIN articlesFts ON (articles.title = articlesFts.title) WHERE articlesFts MATCH :query LIMIT 1")
-    suspend fun searchSavedArticle(query: String?): ArticleEntity
+    suspend fun searchSavedArticle(query: String?): ArticleEntity?
 
     @Query("DELETE  FROM articles  WHERE articles.title = :title")
     suspend fun delete(title: String): Int
